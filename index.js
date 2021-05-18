31f4bde7a76bcb906b809dd60d7d6af7bb7bee06
MdTasib <ohidulalam442@gmail.com>
2021-05-18 12:48:21 +0600

    Complete getWeatherData() to return json response Promise

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * Weather App
- * TODO: Complete getWeatherData() to return json response Promise
+ * DONE: Complete getWeatherData() to return json response Promise
  * TODO: Complete searchCity() to get user input and get data using getWeatherData()
  * TODO: Complete showWeatherData() to set the data in the the html file from response
  */
@@ -12,14 +12,20 @@ let API_KEY = "ca513d9373507b97690f901a262514bc";
  * Retrieve weather data from openweathermap
  * HINT: Use fetch()
  * HINT: URL should look like this: 
- * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
+ * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=ca513d9373507b97690f901a262514bc&units=imperial
  */
 getWeatherData = (city) => {
     const URL = "https://api.openweathermap.org/data/2.5/weather";
     //HINT: Use template literals to create a url with input and an API key
-
+    FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`
     //CODE GOES HERE
+    const weatherPromise = fetch(FULL_URL);
+    return weatherPromise.then(response => {
+        return response.json();
+    })
 }
+
+console.log(getWeatherData('Bangladesh'));
 
 /**
  * Retrieve city input and get the weather data
